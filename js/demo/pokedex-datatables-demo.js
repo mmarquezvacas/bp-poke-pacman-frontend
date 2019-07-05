35d821d63090fae878d19af4b3ab056265464bca
David <david@Davids-MacBook-Pro-4.local>
2019-07-05 16:58:28 -0500

    add filtering

--- js/demo/pokedex-datatables-demo.js
+++ js/demo/pokedex-datatables-demo.js
@@ -1,5 +1,6 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTableExistentes').DataTable();
-  $('#dataTableAtrapados').DataTable();
+$(document).ready(function () {
+  const dtCatched = $('#dataTableAtrapados').DataTable();
+
+  document.getElementById('dataTableExistentes').removeChild(document.getElementById('dataTableExistentes').childNodes[3])
 });
