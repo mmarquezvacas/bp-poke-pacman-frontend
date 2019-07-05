6af2b00ac730e7c8e5e7a471b27ac60cf9ac6717
David <david@Davids-MacBook-Pro-4.local>
2019-07-05 17:40:21 -0500

    add lists

--- js/demo/pokedex-datatables-demo.js
+++ js/demo/pokedex-datatables-demo.js
@@ -1,6 +1,6 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function () {
-  const dtCatched = $('#dataTableAtrapados').DataTable();
+// $(document).ready(function () {
 
-  document.getElementById('dataTableExistentes').removeChild(document.getElementById('dataTableExistentes').childNodes[3])
-});
+//   document.getElementById('dataTableExistentes').removeChild(document.getElementById('dataTableExistentes').childNodes[3])
+//   document.getElementById('dataTableAtrapados').removeChild(document.getElementById('dataTableAtrapados').childNodes[3])
+// });
